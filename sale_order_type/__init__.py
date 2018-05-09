31c5f372e4d08e7f59883a0a902c7477c6a70c87
Dario Lodeiros <dario@aloxa.eu>
2018-05-09 13:31:33 +0200

    [IMP] Copytright

--- sale_order_type/__init__.py
+++ sale_order_type/__init__.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
+#  see __openerp__.py file in root directory
 ##############################################################################
 
 from . import models
